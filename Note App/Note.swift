af812defe6a45ff0bfa77277878b1b36b4a157da
Ibragim Tanshanlo <tang.ibo@hotmail.com>
2023-05-04 23:44:06 +0600

    Initial Commit

---
+++ Note App/Note.swift
@@ -0,0 +1,21 @@
+//
+//  Note.swift
+//  Journal App
+//
+//  Created by Christopher Ching on 2020-02-24.
+//  Copyright © 2020 Christopher Ching. All rights reserved.
+//
+
+import Foundation
+
+struct Note {
+    
+    var docId:String
+    var title:String
+    var body:String
+    var isStarred:Bool
+    var createdAt:Date
+    var lastUpdatedAt:Date
+    
+    
+}
